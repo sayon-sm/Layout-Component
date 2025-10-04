62cc10ae09dc92f772119f8e0c70fbc653332256
sm <its.sayon.mitra@gmail.com>
2025-10-05 00:50:20 +0530

    completed pagination

--- pagination.js
+++ pagination.js
@@ -6,7 +6,6 @@ const btnRight = document.getElementById('next');
 
 btnLeft.addEventListener('click', () => {
   for (i = 1; i < link.length; i++) {
-    console.log(i);
     if (link[i].classList.contains('curr')) {
       link[i - 1].classList.add('curr');
       window.open(link[i - 1].href, '_self');
@@ -18,7 +17,6 @@ btnLeft.addEventListener('click', () => {
 
 btnRight.addEventListener('click', () => {
   for (i = 0; i < link.length - 1; i++) {
-    console.log(i);
     if (link[i].classList.contains('curr')) {
       link[i + 1].classList.add('curr');
       window.open(link[i + 1].href, '_self');
@@ -27,3 +25,12 @@ btnRight.addEventListener('click', () => {
     }
   }
 });
+
+for (a of link) {
+  a.addEventListener('click', clk.bind(this, a));
+}
+
+function clk(a) {
+  document.querySelector('.curr').classList.remove('curr');
+  a.classList.add('curr');
+}
